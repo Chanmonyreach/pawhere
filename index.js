33a7879b8ef48faa133726aa2f47b2456380bf45
Chanmonyreach <131956359+Chanmonyreach@users.noreply.github.com>
2025-05-30 22:40:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 8081;
 
 // Load your Firebase service account key
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
